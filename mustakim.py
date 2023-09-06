d51c962e8a4d35fe5bb34ad8cf03512caba7e4fb
mustakimalby <143859974+mustakimalby@users.noreply.github.com>
2023-09-06 13:14:44 +0700

    Add files via upload

---
+++ mustakim.py
@@ -0,0 +1,23 @@
+print ("........konversi suhu celcious ke farenheit........")
+h = int(input("nilai celciosus adalah = "))
+a = (h* 9/5)+32
+print ("nilai fahrenheit adalah = ", a)
+
+# soal 2
+
+print ("........konversi suhu fahrenheit ke celcious........")
+h = float(input("nilai fahrenheit adalah = "))
+a = (h-32)*5/9
+print ("nilai celcious adalah = ", a)
+
+# soal 3
+print ("........konversi celcious ke kelvin........")
+h = float(input("nilai celcious adalah = "))
+a = h+273.15
+print ("nilai kelvin adalah = ", a)
+
+# soal 4
+print ("........konversi kelvin ke celsious........")
+h = float(input("nilai kelvin adalah = "))
+a = (h - 273.15)
+print ("nilai celcious adalah = ", a)
